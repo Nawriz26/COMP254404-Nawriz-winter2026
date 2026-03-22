04385cc9039178c4295dc7177862b93eff6df50a
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-03-22 03:08:22 -0400

    Add main in Exercise1BinaryTree.java for testing

---
+++ NawrizIbrahim_COMP254_Lab5/src/exercise1/Exercise1Test.java
@@ -0,0 +1,16 @@
+/*
+ ** File:            Exercise1Test.java
+ ** Student:         Nawriz Ibrahim
+ ** Student number:  301161181
+ ** Assignment:      ab Assignment #5 – Using Trees and Priority Queues
+ ** Date:            March 22, 2026
+ ** Description:     Tests Exercise 1: inorderNext(p)
+ */
+
+package exercise1;
+
+public class Exercise1Test {
+    public static void main(String[] args) {
+        
+    }
+}
