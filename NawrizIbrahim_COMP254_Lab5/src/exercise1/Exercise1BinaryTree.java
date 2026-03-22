fc77c67c0538565b3e858ea37e42db3702c17ea0
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-03-22 03:03:45 -0400

    Add Exercise1BinaryTree.java

---
+++ NawrizIbrahim_COMP254_Lab5/src/exercise1/Exercise1BinaryTree.java
@@ -0,0 +1,13 @@
+/*
+ ** File:            Exercise1BinaryTree.java
+ ** Student:         Nawriz Ibrahim
+ ** Student number:  301161181
+ ** Assignment:      ab Assignment #5 – Using Trees and Priority Queues
+ ** Date:            March 22, 2026
+ ** Description:     Binary tree implementation for Exercise 1 using inorderNext(p)
+ */
+
+package exercise1;
+
+public class Exercise1BinaryTree<E> {
+}
