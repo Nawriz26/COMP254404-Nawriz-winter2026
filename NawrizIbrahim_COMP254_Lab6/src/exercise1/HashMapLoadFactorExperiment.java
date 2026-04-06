33c7e7717a5cdf1ce0aba6169ecb96b03edfae40
Nawriz26 <nibrah26@my.centennialcollege.ca>
2026-04-06 10:06:09 -0400

    Project Setup and Add Code Examples 10 required for Exercise 1

---
+++ NawrizIbrahim_COMP254_Lab6/src/exercise1/HashMapLoadFactorExperiment.java
@@ -0,0 +1,13 @@
+/*
+ ** File:            HashMapLoadFactorExperiment.java
+ ** Student:         Nawriz Ibrahim
+ ** Student number:  301161181
+ ** Assignment:      ab Assignment #6 – XXXXXXXXXXXXX
+ ** Date:            April 6, 2026
+ ** Description:     XXXXXXXXXXXX
+ */
+
+package exercise1;
+
+public class HashMapLoadFactorExperiment {
+}
